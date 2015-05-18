c68fdde2d94d451510e64bc08763000372bf4582
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2015-05-18 20:17:28 +0530

    asd

---
+++ swati/test.cpp
@@ -0,0 +1,9 @@
+//
+//  test.cpp
+//  swati
+//
+//  Created by Siddharth on 18/05/15.
+//  Copyright (c) 2015 Siddharth. All rights reserved.
+//
+
+#include "test.h"
---
+++ swati/test.h
@@ -0,0 +1,14 @@
+//
+//  test.h
+//  swati
+//
+//  Created by Siddharth on 18/05/15.
+//  Copyright (c) 2015 Siddharth. All rights reserved.
+//
+
+#ifndef __swati__test__
+#define __swati__test__
+
+#include <stdio.h>
+
+#endif /* defined(__swati__test__) */
