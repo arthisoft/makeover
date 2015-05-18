1202b0dbaa9b06c08c7a18a6bc3eb5912bbe51d1
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2015-05-18 20:12:35 +0530

    sdfsdf

---
+++ swati/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  swati
+//
+//  Created by Siddharth on 18/05/15.
+//  Copyright (c) 2015 Siddharth. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
