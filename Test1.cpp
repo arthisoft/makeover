ded7c21d693d58a027a71ee4005f327d13fa63f6
Siddharth <siddharth@Siddharths-MacBook-Pro.local>
2015-05-18 20:43:25 +0530

    sadf

---
+++ Test1.cpp
@@ -0,0 +1,9 @@
+//
+//  Test1.cpp
+//  swati
+//
+//  Created by Siddharth on 18/05/15.
+//  Copyright (c) 2015 Siddharth. All rights reserved.
+//
+
+#include "Test1.h"
---
+++ Test1.h
@@ -0,0 +1,14 @@
+//
+//  Test1.h
+//  swati
+//
+//  Created by Siddharth on 18/05/15.
+//  Copyright (c) 2015 Siddharth. All rights reserved.
+//
+
+#ifndef __swati__Test1__
+#define __swati__Test1__
+
+#include <stdio.h>
+
+#endif /* defined(__swati__Test1__) */
